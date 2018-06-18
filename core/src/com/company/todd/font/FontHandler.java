f8c38f459c3b58754f1047524d879be57a2b9139
AdvancerMan <advancerman@gmail.com>
2018-06-18 18:14:16 +0700

    added Button class and function that calculates text size

--- core/src/com/company/todd/font/FontHandler.java
+++ core/src/com/company/todd/font/FontHandler.java
@@ -3,13 +3,17 @@ package com.company.todd.font;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 import com.company.todd.ToddEthottGame;
 
 import java.io.File;
+import java.security.KeyPair;
 
-public class FontGenerator {
+public class FontHandler {
+    private static GlyphLayout layout = new GlyphLayout();
+
     public static BitmapFont generateFont(final String fileName, final int size, final Color color) {
         StringBuilder characters = new StringBuilder();
         for (char i = 21; i <= 127; i++) characters.append(i); // цифры, весь английский и служебные символы
@@ -31,5 +35,19 @@ public class FontGenerator {
         generator.dispose();
 
         return font;
+    }
+
+    public static FontSize getTextSize(BitmapFont font, String text) {
+        layout.setText(font, text);
+        return new FontSize(layout.width, layout.height);
+    }
+
+    public static class FontSize {
+        public float width, height;
+
+        FontSize(float width, float height) {
+            this.width = width;
+            this.height = height;
+        }
     }
 }
---
+++ core/src/com/company/todd/menu/Button.java
@@ -0,0 +1,65 @@
+package com.company.todd.menu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import com.company.todd.ToddEthottGame;
+
+public abstract class Button {
+    public static final String BUTTON_FILE = "white.png"; // TODO button texture
+
+    protected ToddEthottGame game;
+    protected ButtonFunction<ToddEthottGame> func;
+    protected Sprite spriteClicked, spriteNotClicked;
+    protected boolean clicked;
+
+    public Button(ButtonFunction<ToddEthottGame> func, ToddEthottGame game,
+                  int x, int y, int width, int height) {
+        this.func = func;
+        this.game = game;
+        clicked = false;
+        TextureRegion clickedTextureRegion = game.textureManager.getTextureRegion(
+                BUTTON_FILE, 5, 5, 200, 20  // TODO button texture
+        );
+        TextureRegion notClickedTextureRegion = game.textureManager.getTextureRegion(
+                BUTTON_FILE, 5, 25, 200, 20  // TODO button texture
+        );
+
+        spriteClicked = new Sprite(clickedTextureRegion);
+        spriteNotClicked = new Sprite(notClickedTextureRegion);
+
+        spriteClicked.setPosition(x, y);
+        spriteNotClicked.setPosition(x, y);
+        spriteClicked.setSize(width, height);
+        spriteNotClicked.setSize(width, height);
+    }
+
+    public void handleInput(float x, float y) {
+        if (Gdx.input.justTouched()) {
+            if (spriteNotClicked.getBoundingRectangle().contains(x, y)) {
+                func.click(game);
+                clicked = true;
+            }
+        }
+    }
+
+    public void update(float delta, float touchX, float touchY) {
+        handleInput(touchX, touchY);
+    }
+
+    public void draw(SpriteBatch batch) {
+        if (clicked) {
+            spriteClicked.draw(batch);
+            clicked = false;
+        }
+        else {
+            spriteNotClicked.draw(batch);
+        }
+    }
+
+    public void dispose() {
+        game.textureManager.disposeTexture(BUTTON_FILE, 2);
+    }
+}
