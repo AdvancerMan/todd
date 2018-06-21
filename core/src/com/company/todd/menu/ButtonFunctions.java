4d795e48b7d791980904da293a7d6d7d2d710133
AdvancerMan <advancerman@gmail.com>
2018-06-22 00:50:29 +0700

    created some button functions

---
+++ core/src/com/company/todd/menu/ButtonFunctions.java
@@ -0,0 +1,36 @@
+package com.company.todd.menu;
+
+import com.company.todd.game.Save;
+import com.company.todd.launcher.ToddEthottGame;
+import com.company.todd.screen.GameScreen;
+import com.company.todd.screen.MainMenuScreen;
+
+public class ButtonFunctions { // TODO more ButtonFunctions
+    public static class RemoveScreenFunction implements ButtonFunction<ToddEthottGame> {
+        @Override
+        public void click(ToddEthottGame game) {
+            game.screenManager.removeScreen();
+        }
+    }
+
+    public static class AddGameScreenFunction implements ButtonFunction<ToddEthottGame> {
+        @Override
+        public void click(ToddEthottGame game) {
+            game.screenManager.addScreen(new GameScreen(game));
+        }
+    }
+
+    public static class AddSavedGameScreenFunction implements ButtonFunction<ToddEthottGame> {
+        @Override
+        public void click(ToddEthottGame game) {
+            game.screenManager.addScreen(new GameScreen(game, new Save()));
+        }
+    }
+
+    public static class AddMainMenuScreenFunction implements ButtonFunction<ToddEthottGame> {
+        @Override
+        public void click(ToddEthottGame game) {
+            game.screenManager.addScreen(new MainMenuScreen(game));
+        }
+    }
+}
