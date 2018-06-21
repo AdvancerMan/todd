c00740c8bbdd6d941cc04972e880f9f6be914302
AdvancerMan <advancerman@gmail.com>
2018-06-22 00:52:13 +0700

    added ButtonsMenu

---
+++ core/src/com/company/todd/menu/ButtonsMenu.java
@@ -0,0 +1,40 @@
+package com.company.todd.menu;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.company.todd.launcher.ToddEthottGame;
+
+import java.util.Iterator;
+
+public class ButtonsMenu {
+    private Array<Button> buttons;
+
+    public ButtonsMenu(ToddEthottGame game, Array<String> buttonTextArray,
+                       Array<ButtonFunction<ToddEthottGame>> buttonFunctions,
+                       int x, int y, int width, int height, int spaceBetweenButtons) {
+        int buttonWidth = width;
+        int buttonHeight = (height - spaceBetweenButtons * (buttonFunctions.size - 1)) / buttonFunctions.size;
+
+        buttons = new Array<Button>();
+        Iterator<ButtonFunction<ToddEthottGame>> functionIterator = buttonFunctions.iterator();
+        Iterator<String> textIterator = buttonTextArray.iterator();
+
+        while (functionIterator.hasNext() && textIterator.hasNext()) {
+            buttons.add(new TextButton(functionIterator.next(), game,
+                    x, y, buttonWidth, buttonHeight, textIterator.next()));
+            y += height + spaceBetweenButtons;
+        }
+    }
+
+    public void update(float delta, float touchX, float touchY) {
+        for (Button button : buttons) {
+            button.update(delta, touchX, touchY);
+        }
+    }
+
+    public void draw(SpriteBatch batch) {
+        for (Button button : buttons) {
+            button.draw(batch);
+        }
+    }
+}
