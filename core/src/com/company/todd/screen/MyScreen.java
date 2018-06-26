4abc579b90f86ce87692d5f43c15921fbab8210e
AdvancerMan <advancerman@gmail.com>
2018-06-26 17:16:26 +0700

    updated MyScreen logic

--- core/src/com/company/todd/screen/MyScreen.java
+++ core/src/com/company/todd/screen/MyScreen.java
@@ -1,23 +1,36 @@
 package com.company.todd.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.company.todd.launcher.ToddEthottGame;
 
 public abstract class MyScreen implements Screen {
     protected ToddEthottGame game;
     protected OrthographicCamera camera;
+    protected Vector3 touchPos;
 
     public MyScreen(ToddEthottGame game) {
         this.game = game;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, ToddEthottGame.WIDTH, ToddEthottGame.HEIGHT);
+
+        touchPos = new Vector3();
     }
 
-    protected abstract void update(float delta);
-    protected abstract void draw(SpriteBatch batch);
+    protected void update(float delta) {
+        camera.update();
+
+        touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+        camera.unproject(touchPos);
+    }
+
+    protected void draw(SpriteBatch batch) {
+        batch.setProjectionMatrix(camera.combined);
+    }
 
     @Override
     public void render(float delta) {
