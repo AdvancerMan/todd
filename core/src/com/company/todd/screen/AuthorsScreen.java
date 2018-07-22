a8253fae1e818bdaf33741ecf6418b938a78a411
AdvancerMan <advancerman@gmail.com>
2018-07-22 19:57:14 +0300

    created classes for settings and authors screens (in development!)

---
+++ core/src/com/company/todd/screen/AuthorsScreen.java
@@ -0,0 +1,26 @@
+package com.company.todd.screen;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.company.todd.launcher.ToddEthottGame;
+
+public class AuthorsScreen extends MyScreen {  // TODO AuthorsScreen
+    public AuthorsScreen(ToddEthottGame game) {
+        super(game);
+    }
+
+    @Override
+    protected void update(float delta) {
+        super.update(delta);
+    }
+
+    @Override
+    protected void draw(SpriteBatch batch) {
+        super.draw(batch);
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/company/todd/screen/SettingsScreen.java
@@ -0,0 +1,26 @@
+package com.company.todd.screen;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.company.todd.launcher.ToddEthottGame;
+
+public class SettingsScreen extends MyScreen {  // TODO SettingsScreen
+    public SettingsScreen(ToddEthottGame game) {
+        super(game);
+    }
+
+    @Override
+    protected void update(float delta) { // TODO reset game progress
+        super.update(delta);
+    }
+
+    @Override
+    protected void draw(SpriteBatch batch) {
+        super.draw(batch);
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
