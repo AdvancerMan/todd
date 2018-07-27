e9046e45109bf0aada45b3b97a45d7381ec54e2f
AdvancerMan <advancerman@gmail.com>
2018-07-27 20:25:56 +0700

    now Platform class extends StaticObject class

--- core/src/com/company/todd/game/static_objs/Platform.java
+++ core/src/com/company/todd/game/static_objs/Platform.java
@@ -1,4 +1,27 @@
 package com.company.todd.game.static_objs;
 
-public class Platform {  // TODO Platform
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+import com.company.todd.launcher.ToddEthottGame;
+
+public class Platform extends StaticObject {  // TODO Platform
+    public Platform(ToddEthottGame game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void draw(SpriteBatch batch, Rectangle cameraRectangle) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
