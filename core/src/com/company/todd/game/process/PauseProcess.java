6a3f267fa6ce90c764c98fc99ea59c785fe6ae02
AdvancerMan <advancerman@gmail.com>
2018-07-27 20:11:01 +0700

    created class PauseProcess to show pause menu on GameScreen

---
+++ core/src/com/company/todd/game/process/PauseProcess.java
@@ -0,0 +1,33 @@
+package com.company.todd.game.process;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.company.todd.launcher.ToddEthottGame;
+import com.company.todd.screen.MyScreen;
+
+public class PauseProcess implements Process {  // TODO PauseScreen
+    private final ToddEthottGame game;
+    private final MyScreen screen;
+    private final GameProcess gameProcess;
+
+    public PauseProcess(ToddEthottGame game, MyScreen screen, GameProcess gameProcess) {
+        this.game = game;
+        this.screen = screen;
+        this.gameProcess = gameProcess;
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        gameProcess.draw(batch);
+        // TODO draw some menu buttons
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
