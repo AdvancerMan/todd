5a2095dadf57a8825cde3a08360a283ebbab301c
AdvancerMan <advancerman@gmail.com>
2018-08-15 15:56:54 +0700

    updated DebugTimer

--- core/src/com/company/todd/debug/DebugTimer.java
+++ core/src/com/company/todd/debug/DebugTimer.java
@@ -1,5 +1,6 @@
 package com.company.todd.debug;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class DebugTimer {
@@ -27,11 +28,31 @@ public class DebugTimer {
     }
 
     public void finish() {
+        finish(0, "time in nanos");
+    }
+
+    public void finishInMs() {
+        finish(-6, "time in ms");
+    }
+
+    public void finishInSeconds() {
+        finish(-9, "time in seconds");
+    }
+
+    public void finishInSeconds(int frames) {
+        finish(-9, 1f / frames, "time in seconds * " + frames + " frames");
+    }
+
+    protected void finish(int exp10, String message) {
+        finish(exp10, 1, message);
+    }
+
+    protected void finish(int exp10, float mul, String message) {
         if (startTime != 0) {
             workingTime += TimeUtils.nanoTime() - startTime;
         }
 
-        System.out.println(name + " debug time in nanos: " + workingTime);
+        System.out.println(name + " " + message + ": " + workingTime * Math.pow(10, exp10) * mul);
 
         workingTime = 0;
         startTime = 0;
