37500ab07622f9b3410c2a39b2242136d06427e8
AdvancerMan <advancerman@gmail.com>
2018-08-15 14:52:14 +0700

    implemented Level class

--- core/src/com/company/todd/game/level/Level.java
+++ core/src/com/company/todd/game/level/Level.java
@@ -1,4 +1,42 @@
 package com.company.todd.game.level;
 
-public class Level {  // TODO Level
+import com.badlogic.gdx.utils.Disposable;
+
+import com.company.todd.game.objs.static_objs.StaticObject;
+import com.company.todd.game.process.GameProcess;
+import com.company.todd.launcher.ToddEthottGame;
+
+import java.util.Stack;
+
+public class Level implements Disposable {  // TODO Level
+    protected ToddEthottGame game;
+    protected GameProcess gameProcess;
+
+    protected Stack<StaticObject> objects;
+
+    public Level(ToddEthottGame game, GameProcess gameProcess) {
+        this.game = game;
+        this.gameProcess = gameProcess;
+    }
+
+    public void addObject(StaticObject object) {
+        objects.push(object);
+    }
+
+    public boolean hasNext() {
+        return objects.size() > 0;
+    }
+
+    public StaticObject nextObject() {
+        return objects.pop();
+    }
+
+    // TODO read level from save
+
+    @Override
+    public void dispose() {
+        while (hasNext()) {
+            nextObject().dispose();
+        }
+    }
 }
