1764c0708dd924eba01cd90bb33b5c83c2da7f6a
AdvancerMan <advancerman@gmail.com>
2019-08-22 20:05:50 +0700

    created AirBomb

---
+++ core/src/com/company/todd/game/objs/active_objs/dangerous/objects/AirBomb.java
@@ -0,0 +1,29 @@
+package com.company.todd.game.objs.active_objs.dangerous.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.company.todd.box2d.BodyInfo;
+import com.company.todd.game.animations.MyAnimation;
+import com.company.todd.game.objs.InGameObject;
+import com.company.todd.game.objs.active_objs.dangerous.DangerousObject;
+import com.company.todd.launcher.ToddEthottGame;
+
+public class AirBomb extends DangerousObject {
+    public AirBomb(ToddEthottGame game, InGameObject owner, MyAnimation animation,
+                   float damage, float spriteWidth, float spriteHeight,
+                   float x, float y, float bodyWidth, float bodyHeight) {
+        super(game, owner, animation, 0, damage, new Vector2(spriteWidth, spriteHeight),
+                new BodyInfo(x, y, bodyWidth, bodyHeight));
+    }
+
+    public AirBomb(ToddEthottGame game, InGameObject owner, MyAnimation animation,
+                   float damage, float x, float y, float width, float height) {
+        this(game, owner, animation, damage, width, height, x, y, width, height);
+    }
+
+    @Override
+    public void takeDamage(float amount) {
+        kill();
+    }
+}
