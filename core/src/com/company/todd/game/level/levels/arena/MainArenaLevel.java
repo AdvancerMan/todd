8c59a1a9ba422cac872031fe2dc58694a1c23f7b
AdvancerMan <advancerman@gmail.com>
2019-09-03 13:32:54 +0300

    created MainArenaLevel class

---
+++ core/src/com/company/todd/game/level/levels/arena/MainArenaLevel.java
@@ -0,0 +1,17 @@
+package com.company.todd.game.level.levels.arena;
+
+import com.company.todd.game.level.Level;
+import com.company.todd.game.objs.static_objs.Platform;
+import com.company.todd.launcher.ToddEthottGame;
+
+public class MainArenaLevel extends Level {
+    public MainArenaLevel(ToddEthottGame game) {
+        super(game);
+
+        createLevel();
+    }
+
+    private void createLevel() {
+        // TODO createLevel()
+    }
+}
