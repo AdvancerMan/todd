bde0e987ee6258540fc27fccf69dcad0ea6e8f6a
AdvancerMan <advancerman@gmail.com>
2019-09-03 23:33:23 +0300

    made HalfCollidedPlatform

---
+++ core/src/com/company/todd/game/objs/static_objs/HalfCollidedPlatform.java
@@ -0,0 +1,47 @@
+package com.company.todd.game.objs.static_objs;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.utils.Array;
+import com.company.todd.game.animations.MyAnimation;
+import com.company.todd.game.objs.InGameObject;
+import com.company.todd.launcher.ToddEthottGame;
+
+public class HalfCollidedPlatform extends StaticObject {
+    protected Array<InGameObject> notCollidingObjects;
+
+    public HalfCollidedPlatform(ToddEthottGame game, MyAnimation animation,
+                                float x, float y, float width, float height) {
+        super(game, animation, x, y, width, height);
+
+        notCollidingObjects = new Array<InGameObject>();
+    }
+
+    @Override
+    public void beginContact(Contact contact, InGameObject object) {
+        super.beginContact(contact, object);
+
+        float normalAngle = contact.getWorldManifold().getNormal().angle();
+
+        if (!(89f < normalAngle && normalAngle < 91f)) {
+            notCollidingObjects.add(object);
+        }
+    }
+
+    @Override
+    public void contactPreSolve(Contact contact, Manifold oldManifold, InGameObject object) {
+        super.contactPreSolve(contact, oldManifold, object);
+
+        if (notCollidingObjects.contains(object, false)) {
+            contact.setEnabled(false);
+        }
+    }
+
+    @Override
+    public void endContact(Contact contact, InGameObject object) {
+        super.endContact(contact, object);
+
+        while (notCollidingObjects.removeValue(object, false));  // TODO optimize
+    }
+}
