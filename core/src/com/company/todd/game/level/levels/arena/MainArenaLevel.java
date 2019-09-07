2ec3ced66662162b04c95dc41072c8645e7dc681
AdvancerMan <advancerman@gmail.com>
2019-09-07 23:59:19 +0300

    created some platforms on main level

--- core/src/com/company/todd/game/level/levels/arena/MainArenaLevel.java
+++ core/src/com/company/todd/game/level/levels/arena/MainArenaLevel.java
@@ -1,7 +1,19 @@
 package com.company.todd.game.level.levels.arena;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.utils.Array;
+import com.company.todd.game.animations.MyAnimation;
 import com.company.todd.game.level.Level;
+import com.company.todd.game.objs.InGameObject;
+import com.company.todd.game.objs.static_objs.HalfCollidedPlatform;
+import com.company.todd.game.objs.static_objs.PlatformWithUpperLayer;
+import com.company.todd.game.objs.static_objs.StaticObject;
 import com.company.todd.launcher.ToddEthottGame;
+import com.company.todd.texture.AnimationInfo;
+
+import static com.company.todd.box2d.BodyCreator.addPolygon;
+import static com.company.todd.box2d.BodyCreator.createBody;
 
 public class MainArenaLevel extends Level {
     public MainArenaLevel(ToddEthottGame game) {
@@ -11,6 +23,58 @@ public class MainArenaLevel extends Level {
     }
 
     private void createLevel() {
-        // TODO createLevel()
+        AnimationInfo halfColAnim = game.animationInfos.getAnimationInfo("tmp"); // todo halfCol anim
+
+        Array<StaticObject> objects = new Array<StaticObject>(new StaticObject[]{
+                new HalfCollidedPlatform(game, null, -1000, -10, 2000, 10),
+                new StaticObject(game, null, 0, 0, 0, 0, 500, 750) {
+                    @Override
+                    protected void createMyBody() {
+                        destroyMyBody();
+
+                        body = createBody(gameProcess.getWorld(), BodyDef.BodyType.StaticBody, new Vector2(-1125, 290));
+                        addPolygon(body, new float[] {
+                                -225, -300,
+                                225, -300,
+                                225, -150,
+                                -75, 150,
+                                -225, 150
+                        });
+                        addPolygon(body, new float[] {
+                                -75, 150,
+                                -75, 300,
+                                -225, 300,
+                                -225, 150
+                        });
+                        body.setUserData(this);
+                    }
+                },
+                new StaticObject(game, null, 0, 0, 0, 0, 500, 750) {
+                    @Override
+                    protected void createMyBody() {
+                        destroyMyBody();
+
+                        body = createBody(gameProcess.getWorld(), BodyDef.BodyType.StaticBody, new Vector2(1125, 290));
+                        addPolygon(body, new float[] {
+                                225, -300,
+                                -225, -300,
+                                -225, -150,
+                                75, 150,
+                                225, 150
+                        });
+                        addPolygon(body, new float[] {
+                                75, 150,
+                                75, 300,
+                                225, 300,
+                                225, 150
+                        });
+                        body.setUserData(this);
+                    }
+                }
+        });
+
+        for (StaticObject object : objects) {
+            addObject(object);
+        }
     }
 }
