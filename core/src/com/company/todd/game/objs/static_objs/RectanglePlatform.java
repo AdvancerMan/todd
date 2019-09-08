a8485a57064b3cb034d6d6eeb226832d3f97f6b0
AdvancerMan <advancerman@gmail.com>
2019-09-08 22:44:16 +0300

    created RectanglePlatform

---
+++ core/src/com/company/todd/game/objs/static_objs/RectanglePlatform.java
@@ -0,0 +1,13 @@
+package com.company.todd.game.objs.static_objs;
+
+import com.company.todd.game.animations.MyAnimation;
+import com.company.todd.launcher.ToddEthottGame;
+
+public class RectanglePlatform extends StaticObject {
+    public RectanglePlatform(ToddEthottGame game, MyAnimation animation,
+                             float x, float y,
+                             float bodyWidth, float bodyHeight,
+                             float spriteWidth, float spriteHeight) {
+        super(game, animation, x, y, bodyWidth, bodyHeight, spriteWidth, spriteHeight);
+    }
+}
