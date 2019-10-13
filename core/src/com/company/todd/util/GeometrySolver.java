6c828267ffa40cbac81b3089025ac3e0a83ec358
AdvancerMan <advancerman@gmail.com>
2019-10-14 00:39:35 +0300

    created GeometrySolver and added some methods

---
+++ core/src/com/company/todd/util/GeometrySolver.java
@@ -0,0 +1,43 @@
+package com.company.todd.util;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class GeometrySolver {
+    public static float pointToSegmentDist2(
+            Vector2 segmentStart, Vector2 segmentEnd, Vector2 point
+    ) {
+        Vector2 segmentVector = segmentEnd.cpy().sub(segmentStart);
+        Vector2 pointFromStartVector = point.cpy().sub(segmentStart);
+
+        float dot = segmentVector.dot(pointFromStartVector);
+        if (dot < 0) {
+            return point.dst2(segmentStart);
+        }
+
+        float len2 = segmentVector.len2();
+        if (len2 < dot) {
+            return point.dst2(segmentEnd);
+        }
+
+        float crs = segmentVector.crs(pointFromStartVector);
+        return Math.abs(crs * crs / segmentVector.len2());
+    }
+
+    public static double pointToSegmentDist(
+            Vector2 segmentStart, Vector2 segmentEnd, Vector2 point
+    ) {
+        return Math.sqrt((double) pointToSegmentDist2(segmentStart, segmentEnd, point));
+    }
+
+    public static boolean isSegmentContainsPoint(
+            Vector2 segmentStart, Vector2 segmentEnd, Vector2 point, float eps
+    ) {
+        return pointToSegmentDist(segmentStart, segmentEnd, point) < eps;
+    }
+
+    public static boolean isSegmentContainsPoint(
+            Vector2 segmentStart, Vector2 segmentEnd, Vector2 point
+    ) {
+        return isSegmentContainsPoint(segmentStart, segmentEnd, point, FloatCmp.EPS);
+    }
+}
